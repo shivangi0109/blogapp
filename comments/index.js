816d034d352f127dfff4371a9d7d9a68f6678452
Shivangi Srivastava <shi06dec@gmail.com>
2024-08-12 15:55:35 -0600

    Handle cors errors

--- comments/index.js
+++ comments/index.js
@@ -2,9 +2,11 @@ const PORT = 4001;
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
