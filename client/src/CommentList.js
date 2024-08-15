6a93e2ee8ce32f7a93e2cbfb512b1b3b4dfa1fc7
Shivangi Srivastava <shi06dec@gmail.com>
2024-08-15 16:21:33 -0600

    Use the query service in the client side to fetch all of the posts

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,18 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = async () => {
-    const result =  await axios.get(`http://localhost:4001/posts/${postId}/comments`);
-
-    setComments(result.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+const CommentList = ({ comments }) => {
 
   const renderedComments = comments.map((comment) => {
     return (
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,8 +7,8 @@ const PostList = () => {
   const [posts, setPosts] = useState({});
 
   const fetchPosts = async () => {
-    const result = await axios.get("http://localhost:4000/posts");
-
+    const result = await axios.get("http://localhost:4002/posts");
+    console.log(result.data);
     setPosts(result.data);
   };
 
@@ -21,7 +21,7 @@ const PostList = () => {
       <div key={post.id} className="card" style={{ width: '30%', marginBottom: '20px' }}>
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
