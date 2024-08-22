c7a53261a7bc6b6566db379d68423fb8e4f14a5b
Shivangi Srivastava <shi06dec@gmail.com>
2024-08-22 14:44:34 -0600

    Rendering comments by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,9 +1,23 @@
 const CommentList = ({ comments }) => {
 
   const renderedComments = comments.map((comment) => {
+    let content;
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    }
+
+    if (comment.status === 'pending') {
+      content = 'This content is awaiting moderation';
+    }
+
+    if (comment.status === 'rejected') {
+      content = 'This content has been rejected';
+    }
+
     return (
       <li key={comment.id}>
-        {comment.content}
+        {content}
       </li>
     );
   });
