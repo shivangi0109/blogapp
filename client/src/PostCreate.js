d03d39bbfa15ca22f9f2815ca8993b2d5eae1194
Shivangi Srivastava <shi06dec@gmail.com>
2024-10-24 16:26:55 -0600

    Unique route path for creating a post and getting a posts as ingress doesn't use GET and POST methods

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://posts.com/posts", {
+    await axios.post("http://posts.com/posts/create", {
       title
     });
 
